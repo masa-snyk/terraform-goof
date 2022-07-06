1bf3b02d702e1696044dfbc2ebfa6aa062c8b893
Masa Ito <masatomo.ito@snyk.io>
2022-07-06 11:10:40 +0900

    scripts

--- 1.make_plan_json.sh
+++ 1.make_plan_json.sh
@@ -2,12 +2,5 @@
 
 set -x
 
-#if [ $# -ne 0 ]
-#	then
-#		echo 'Needs to supply argument'
-#		echo '  $1 = <arg>'
-#		exit 1
-#fi
-
 terraform plan -out=tf.tfplan 
 terraform show -json tf.tfplan  > tfplan.json
--- scan-drift-only-unmanaged.sh
+++ scan-drift-only-unmanaged.sh
@@ -2,11 +2,4 @@
 
 set -x
 
-#if [ $# -ne 0 ]
-#	then
-#		echo 'Needs to supply argument'
-#		echo '  $1 = <arg>'
-#		exit 1
-#fi
-
 snyk iac describe --only-unmanaged 
